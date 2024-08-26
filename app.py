f43e3a36b8558bb1a56885952e3b88aef99fd148
lsavoie <twosavoie@gmail.com>
2024-08-26 13:54:17 -0500

    Solution code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -28,40 +28,56 @@ with app.app_context():
 	db.session.commit()
 
 
+#### Serving Static Files
 @app.route("/")
-def villains_cards():
-	return render_template("villain.html", villains=Villain.query.all())
+def villain_cards():
+	return app.send_static_file("villain.html")
 
 
-@app.route("/add", methods=["GET"])
+@app.route("/add")
+def add():
+	return app.send_static_file("addvillain.html")
+
+@app.route("/delete")
+def delete():
+	return app.send_static_file("deletevillain.html")
+
+####
+
+
+@app.route("/api/villains/", methods=["GET"])
+def get_villains():
+	villains = Villain.query.all()
+	data = []
+	return
+
+
+@app.route("/api/villains/add", methods=["POST"])
 def add_villain():
-	return render_template("addvillain.html", errors=[])
-
-@app.route("/delete", methods=["GET"])
-def delete_villain():
-	return render_template("deletevillain.html", errors=[])
-
-@app.route("/addVillain", methods=["POST"])
-def add_user():
 	errors = []
 	name = request.form.get("name")
+
 	if not name:
 		errors.append("Oops! Looks like you forgot a name!")
+
 	description = request.form.get("description")
 	if not description:
 		errors.append("Oops! Looks like you forgot a description!")
+
 	interests = request.form.get("interests")
 	if not interests:
 		errors.append("Oops! Looks like you forgot some interests!")
+
 	url = request.form.get("url")
 	if not url:
 		errors.append("Oops! Looks like you forgot an image!")
+
 	villain = Villain.query.filter_by(name=name).first()
 	if villain:
 		errors.append("Oops! A villain with that name already exists!")
 
 	if errors:
-		return render_template("addvillain.html", errors=errors)
+		return jsonify({"errors": errors})
 	else:
 		new_villain = Villain(name=name,
 		                      description=description,
@@ -69,19 +85,19 @@ def add_user():
 		                      url=url)
 		db.session.add(new_villain)
 		db.session.commit()
-		return render_template("villain.html", villains=Villain.query.all())
+		return
 
-@app.route("/deleteVillain", methods=["POST"])
-def delete_user():
+@app.route("/api/villains/delete", methods=["POST"])
+def delete_villain():
 	name = request.form.get("name")
 	villain = Villain.query.filter_by(name=name).first()
 	if villain:
 		db.session.delete(villain)
 		db.session.commit()
-		return render_template("villain.html", villains=Villain.query.all())
+		return
 	else:
-		return render_template("deletevillain.html",
-		                       errors=["Oops! That villain doesn't exist!"])
+		return jsonify(
+		    {"errors": ["Oops! A villain with that name doesn't exist!"]})
 
 # Run the flask server
 if __name__ == "__main__":
