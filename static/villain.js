e76bb409b17969687776ab1166af0c28781c75c6
Lisa Savoie <twosavoie@gmail.com>
2025-09-10 12:57:30 -0500

    Accessibility updates

--- static/villain.js
+++ static/villain.js
@@ -4,6 +4,6 @@ $.get("/api/", function (data) {
 
 $.get("/api/villains/", function (data) {
   for (let i in data) {
-    $(".content").append('<div class="card"><h2 class="card-name">' + data[i].name + '</h2><img src="' + data[i].url + '" class="card-image"/><div class="card-body"><p class="card-description">' + data[i].description + '</p><h4>Interests</h4><p class="card-interests">' + data[i].interests + '</p><p class="card-date-added">Date added: ' + data[i].date_added + '</p></div></div>')
+    $(".content").append('<div class="card"><h2 class="card-name">' + data[i].name + '</h2><img src="' + data[i].url + '" class="card-image" alt="' + data[i].name + '"/><div class="card-body"><p class="card-description">' + data[i].description + '</p><h3>Interests</h3><p class="card-interests">' + data[i].interests + '</p><p class="card-date-added">Date added: ' + data[i].date_added + '</p></div></div>')
   }
 })
